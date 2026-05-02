477f383969e95e2c506a983148009335a8354b77
Igor Zelenin <iazelenin@icloud.com>
2026-05-02 16:57:16 +0400

    Add file manager

---
+++ src/main/java/com/javarush/zelenin/file/FileManager.java
@@ -0,0 +1,34 @@
+package com.javarush.zelenin.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class FileManager {
+
+    public Stream<String> readFile(String filePath) throws IOException {
+        return Files.lines(getFullPath(filePath));
+    }
+
+    public void writeFile(Stream<String> lines, String filePath) throws IOException {
+        Files.write(getFullPath(filePath), (Iterable<String>) lines::iterator);
+    }
+
+    public static Path constructOutputPath(String filePath, String newFileName) {
+        String originalFile = getFullPath(filePath).getFileName().toString();
+
+        if (originalFile.equals(newFileName + ".txt")) {
+            throw new IllegalArgumentException("Имя нового файла совпадает с именем исходного файла!");
+        }
+        return Path.of(getFullPath(filePath).getParent().toString(), newFileName + ".txt");
+    }
+
+    public static Path getFullPath(String filePath) {
+        String expandedPath = filePath.startsWith("~")
+                ? filePath.replaceFirst("^~", System.getProperty("user.home"))
+                : filePath;
+
+        return Path.of(expandedPath).toAbsolutePath().normalize();
+    }
+}
